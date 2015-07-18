2e7adc9465cd2f5b11bf210963573220c4c20441
16chuang <16chuang@castilleja.org>
2015-07-18 15:49:43 -0700

    Added base model, view, and VC classes.

---
+++ ShareMyList/ShareMyList/Views/ListItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListItemTableViewCell.swift
+//  ShareMyList
+//
+//  Created by Claire Huang on 7/18/15.
+//  Copyright (c) 2015 ShareMyListAwesomeness. All rights reserved.
+//
+
+import UIKit
+
+class ListItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
