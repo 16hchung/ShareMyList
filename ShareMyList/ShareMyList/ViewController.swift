cf62b8916df34476ccfe156119c9698c489defe5
Heejung Chung <16hchung@castilleja.org>
2015-07-18 14:49:52 -0700

    created new Xcode project

---
+++ ShareMyList/ShareMyList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareMyList
+//
+//  Created by Heejung Chung on 7/18/15.
+//  Copyright (c) 2015 ShareMyListAwesomeness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
