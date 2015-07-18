4686bf1ca7534072fd38e1960275a35d0105e390
16chuang <16chuang@castilleja.org>
2015-07-18 16:50:08 -0700

    Added ParseHelper class.

---
+++ ShareMyList/ShareMyList/Helpers/ParseHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ParseHelper.swift
+//  ShareMyList
+//
+//  Created by Claire Huang on 7/18/15.
+//  Copyright (c) 2015 ShareMyListAwesomeness. All rights reserved.
+//
+
+import Foundation
+
+class ParseHelper {
+    
+}
