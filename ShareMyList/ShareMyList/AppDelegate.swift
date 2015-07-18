1a5eaad0991c39a86dbbbb3217c9f5fa9e09f702
Kavya Tewari <kavya.tewari@gmail.com>
2015-07-18 16:18:30 -0700

    Parse framework

--- ShareMyList/ShareMyList/AppDelegate.swift
+++ ShareMyList/ShareMyList/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Parse
+import Bolts
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        Parse.enableLocalDatastore()
+        Parse.setApplicationId("SzV0JFZHfZkez8OBMhWj3BnIn52DrWsRN9V9XiAk", clientKey: "aH4MoGlunP5ZEvPKEPhCKta3XPQHT4razXfd7L3N")
+        
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         // Override point for customization after application launch.
         return true
     }
